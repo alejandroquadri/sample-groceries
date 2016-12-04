ed6c7d5c63c13104f397b47ceefa5b230327b127
Alejandro Quadri <alejandroquadri@gmail.com>
2016-12-04 16:10:51 -0300

    Inicializa fb en el main sin problemas

--- app/main.ts
+++ app/main.ts
@@ -3,5 +3,35 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
 import { setStatusBarColors } from "./utils/status-bar-util";
 
+import firebase = require("nativescript-plugin-firebase");
+
+firebase.init({
+   persist: true
+          }).then(
+              function (instance) {
+                console.log("firebase.init done");
+              },
+              function (error) {
+                console.log("firebase.init error: " + error);
+              }
+          );
+
 setStatusBarColors();
 platformNativeScriptDynamic().bootstrapModule(AppModule);
+
+// import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
+// import { AppModule } from "./app.module";
+// import firebase = require("nativescript-plugin-firebase");
+//
+// firebase.init({
+//    persist: true
+//           }).then(
+//               function (instance) {
+//                 console.log("firebase.init done");
+//               },
+//               function (error) {
+//                 console.log("firebase.init error: " + error);
+//               }
+//           );
+//
+// platformNativeScriptDynamic().bootstrapModule(AppModule);
