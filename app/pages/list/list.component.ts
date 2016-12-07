4761a7a4dd113ee6b5cefdd5bd8104e735d42e2c
Alejandro Quadri <alejandroquadri@gmail.com>
2016-12-07 14:55:56 -0300

    Anda la funcionalidad de agregar y borrar verduras. Falta hacerle algunas pequeñas mejoras

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -15,24 +15,23 @@ export class ListComponent implements OnInit {
   groceryList: Array<Grocery> = [];
   grocery = "";
   isLoading = false;
-  listLoaded = false;
+  listLoaded = true;
+  lista;
 
   @ViewChild("groceryTextField") groceryTextField: ElementRef;
 
   constructor(
     private groceryListService: GroceryListService,
-    private zone: NgZone) {}
+    private zone: NgZone
+  ) {}
 
   ngOnInit() {
-    this.isLoading = true;
-    this.groceryListService.load()
-      .subscribe(loadedGroceries => {
-        loadedGroceries.forEach((groceryObject) => {
-          this.groceryList.unshift(groceryObject);
-        });
-        this.isLoading = false;
-        this.listLoaded = true;
-      });
+    // this.lista = this.groceryListService.groceryList;
+    this.groceryListService.groceryList.subscribe((data) => {
+      // this.lista = JSON.stringify(data);
+      this.lista = data;
+      console.log("Lista", JSON.stringify(this.lista));
+    });
   }
 
   add() {
@@ -46,30 +45,13 @@ export class ListComponent implements OnInit {
     textField.dismissSoftInput();
 
     this.groceryListService.add(this.grocery)
-      .subscribe(
-        groceryObject => {
-          this.groceryList.unshift(groceryObject);
-          this.grocery = "";
-        },
-        () => {
-          alert({
-            message: "An error occurred while adding an item to your list.",
-            okButtonText: "OK"
-          });
-          this.grocery = "";
-        }
-      );
+    .then((ret) => {
+      console.log("vuelve", ret);
+    });
   }
 
-  delete(grocery: Grocery) {
-    this.groceryListService.delete(grocery.id)
-      .subscribe(() => {
-        // Running the array splice in a zone ensures that change detection gets triggered.
-        this.zone.run(() => {
-          let index = this.groceryList.indexOf(grocery);
-          this.groceryList.splice(index, 1);
-        });
-      });
+  delete(grocery) {
+    this.groceryListService.delete(grocery);
   }
 
   share() {
--- app/shared/grocery/grocery-list.service.ts
+++ app/shared/grocery/grocery-list.service.ts
@@ -1,59 +1,64 @@
-import { Injectable } from "@angular/core";
+import { Injectable, NgZone } from "@angular/core";
 import { Http, Headers } from "@angular/http";
 import { Config } from "../config";
 import { Grocery } from "./grocery";
 import { Observable } from "rxjs/Rx";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import "rxjs/add/operator/map";
+
+import firebase = require("nativescript-plugin-firebase");
+// declare var zonedCallback: Function;
 
 @Injectable()
 export class GroceryListService {
-  constructor(private http: Http) {}
+
+  groceryList = new BehaviorSubject([]);
+
+  constructor(
+    private http: Http,
+    private ngZone: NgZone
+  ) {
+    this.load();
+  }
 
   load() {
-    let headers = new Headers();
-    headers.append("Authorization", "Bearer " + Config.token);
-
-    return this.http.get(Config.apiUrl + "Groceries", {
-      headers: headers
-    })
-    .map(res => res.json())
-    .map(data => {
-      let groceryList = [];
-      data.Result.forEach((grocery) => {
-        groceryList.push(new Grocery(grocery.Id, grocery.Name));
+    // firebase.addValueEventListener(zonedCallback(this.onQueryEvent.bind(this)), "/list");
+    firebase.addValueEventListener((result: any) => {
+      this.ngZone.run(() => {
+        this.onQueryEvent(result);
       });
-      return groceryList;
-    })
-    .catch(this.handleErrors);
+    }, "/list")
+    .then(ret => console.log("retorno", JSON.stringify(ret)));
+  }
+
+  onQueryEvent(result: any) {
+    let list = [];
+    if (result.value) {
+      Object.keys(result.value).forEach((key) => {
+        let entry = result.value[key];
+        let newItem = {
+          key: key,
+          name: entry.name
+        };
+        list.push(newItem);
+      });
+    }
+    this.groceryList.next(list);
+    return Promise.resolve();
   }
 
   add(name: string) {
-    let headers = new Headers();
-    headers.append("Authorization", "Bearer " + Config.token);
-    headers.append("Content-Type", "application/json");
-
-    return this.http.post(
-      Config.apiUrl + "Groceries",
-      JSON.stringify({ Name: name }),
-      { headers: headers }
-    )
-    .map(res => res.json())
-    .map(data => {
-      return new Grocery(data.Result.Id, name);
-    })
-    .catch(this.handleErrors);
+    return firebase.push(
+        "/list",
+        { "name": name, "Date": 0 - Date.now() }
+      )
+      .catch(this.handleErrors);
   }
 
-  delete(id: string) {
-    let headers = new Headers();
-    headers.append("Authorization", "Bearer " + Config.token);
-    headers.append("Content-Type", "application/json");
-
-    return this.http.delete(
-      Config.apiUrl + "Groceries/" + id,
-      { headers: headers }
-    )
-    .map(res => res.json())
+  delete(item) {
+    console.log("item id", item.key);
+    return firebase.remove("/list/" + item.key)
+    .then(() => console.log("borrado ", item.key))
     .catch(this.handleErrors);
   }
 
